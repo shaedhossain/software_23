128971e119fa74f82d3ac3b12c97fffbfda2913e
Shad <shadfahim9998@gmail.com>
2025-09-18 22:41:52 +0600

    first commit for bp

---
+++ backPropagation.py
@@ -0,0 +1,14 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+x_train= np.loadtxt('files/train_X.csv', delimiter=',').T
+y_train= np.loadtxt('files/train_label.csv',delimiter=',').T
+
+x_test= np.loadtxt('files/test_X.csv', delimiter=',').T
+y_test= np.loadtxt('files/test_label.csv',delimiter=',').T
+
+print(x_train.shape)
+print(y_train.shape)
+print(x_test.shape)
+print(y_test.shape)
